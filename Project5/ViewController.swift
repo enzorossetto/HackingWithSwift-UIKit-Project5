cdecab9be9e5f95ab0c5b41232c1652ed01455c4
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-07-08 17:44:34 -0300

    Word Scramble - part 4
    Prepare for submission: lowercased() and IndexPath

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -59,7 +59,25 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
         
+        if isPossible(word: lowerAnswer) && isOriginal(word: lowerAnswer) && isReal(word: lowerAnswer) {
+            usedWords.insert(lowerAnswer, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        true
+    }
+    
+    func isReal(word: String) -> Bool {
+        true
     }
 }
 
